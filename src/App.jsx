3735bc3f665b1d49d816735b777ca6ea5c811a66
Vincent_tt <evgeniy161102@gmail.com>
2024-11-06 17:45:00 +0300

    add skeleton for news and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage/MainPage";
 
-// TODO:  https://youtu.be/bD0UXb7kD_k?list=PLu_vAIOaYQ6Q5yLM3gtHmrrtYE-MywJg2&t=1881
+// TODO:  https://youtu.be/FeDSumhe0kQ?list=PLu_vAIOaYQ6Q5yLM3gtHmrrtYE-MywJg2&t=1
 
 function App() {
   return (
