7907bbb265248b5d10d69da16c4e2d1d2afaca07
Vincent_tt <evgeniy161102@gmail.com>
2024-11-13 13:23:25 +0300

    Добавлен поиск - debounce (запрос через 1.5 сек после ввода ключевого слова

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,10 @@
+import styles from "./Search.module.scss"
+
+
+const Search = ({ keywords, setKeywords }) => {
+	return (
+		<div><input type="text" className={styles["input"]} value={keywords} onChange={(e) => setKeywords(e.target.value)} placeholder="Search" /></div>
+	)
+}
+
+export default Search
